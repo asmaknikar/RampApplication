8668f067763b0c4b4930dbd4d8be9e6ac002fc34
asmaknikar <asmaknikar@gmail.com>
2023-03-01 02:49:10 -0800

    Bug 7

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -5,7 +5,7 @@ import { TransactionPane } from "./TransactionPane"
 import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
 
 export const Transactions: TransactionsComponent = ({ transactions }) => {
-  const { fetchWithoutCache, loading } = useCustomFetch()
+  const { fetchWithoutCache, clearCache, loading } = useCustomFetch()
 
   const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
     async ({ transactionId, newValue }) => {
@@ -16,6 +16,7 @@ export const Transactions: TransactionsComponent = ({ transactions }) => {
     },
     [fetchWithoutCache]
   )
+  clearCache();
 
   if (transactions === null) {
     return <div className="RampLoading--container">Loading...</div>
